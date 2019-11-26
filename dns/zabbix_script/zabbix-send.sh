37b406c50a5016b9707ee80402e2d48fb082c463
Tomoyuki KOYAMA <tomoyk@users.noreply.github.com>
2019-11-26 14:14:07 +0900

    Update zabbix-send.sh

--- dns/zabbix_script/zabbix-send.sh
+++ dns/zabbix_script/zabbix-send.sh
@@ -1,10 +1,10 @@
-# PreScript
-/usr/sbin/unbound-control stats | egrep '(num.query.type|mem.cache|total.num)' | sed 's/=/ /g' | sed 's/^/cbnoc-dns /g' > /root/zabbix_script/result
-
 # Conf
 ZABBIX_IPADDR='10.1.1.24'
 ZABBIX_PORT='10051'
 YOUR_HOSTNAME_ON_ZABBIX="cbnoc-dns"
+
+# PreScript
+/usr/sbin/unbound-control stats | egrep '(num.query.type|mem.cache|total.num)' | sed 's/=/ /g' | sed "s/^/$YOUR_HOSTNAME_ON_ZABBIX /g" > /root/zabbix_script/result
 
 # Send
 /usr/bin/zabbix_sender -z ${ZABBIX_IPADDR} \
